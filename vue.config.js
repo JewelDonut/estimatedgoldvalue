f268410dfbeb2d9eb0c63bdce570c7e3355ac6ab
Jewel Donut <coughmedicine2021@gmail.com>
2022-07-22 15:48:55 -0400

    Deploy scripting

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/estimategoldvalue/'
+  publicPath: '/estimatedgoldvalue/'
 })
